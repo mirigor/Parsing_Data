d4e93c2c4cdd18f5c7f04eab3bc5008fe50b0c1b
mirigor <medved-1297@mail.ru>
2021-11-05 22:38:29 +0300

    minor fix

--- test_task/test_task/spiders/catalog.py
+++ test_task/test_task/spiders/catalog.py
@@ -4,13 +4,13 @@ import scrapy
 def price_data(response):
     """Сбор информации об обычной-цене товара"""
 
-    if response.css('.nA .nF span::text').extract_first() == 'Нет в наличии':
+    if response.css('.nb .ng span::text').extract_first() == 'Нет в наличии':
         return
     else:
-        if response.css('.Cd.Ch .Ce').extract():
-            original_price = response.css('.Cd .Cf .Cg::text').extract_first()
+        if response.css('.B_8.Cc .B_9').extract():
+            original_price = response.css('.B_8 .Ca .Cb::text').extract_first()
         else:
-            original_price = response.css('.Cd .Ce::text').extract_first()
+            original_price = response.css('.B_8 .B_9::text').extract_first()
         temp = ''
         for x in str(original_price):
             if x.isdigit() or x == '.':
@@ -22,10 +22,10 @@ def price_data(response):
 def promo_price_data(response):
     """Сбор информации о промо-цене товара"""
 
-    if response.css('.nA .nF span::text').extract_first() == 'Нет в наличии' or not response.css('.Cd.Ch .Ce::text').extract_first():
+    if response.css('.nb .ng span::text').extract_first() == 'Нет в наличии' or not response.css('.B_8.Cc .B_9::text').extract_first():
         return
     else:
-        price = response.css('.Cd.Ch .Ce::text').extract_first()
+        price = response.css('.B_8.Cc .B_9::text').extract_first()
         temp = ''
         for x in str(price):
             if x.isdigit() or x == '.':
@@ -41,8 +41,8 @@ class CatalogSpider(scrapy.Spider):
 
     def parse(self, response, **kwargs):
         for page in range(2, 100):
-            if response.css('.Ma.MC::attr("href")').extract():
-                for href in response.css('.Ma.MC::attr("href")').extract():
+            if response.css('.M_7.Nz::attr("href")').extract():
+                for href in response.css('.M_7.Nz::attr("href")').extract():
                     url = response.urljoin(href)
                     yield scrapy.Request(url, callback=self.parsing_data)
 
@@ -54,7 +54,7 @@ class CatalogSpider(scrapy.Spider):
     def parsing_data(response):
         item = {
             'id': response.css('div::attr(data-product-id)').extract_first(),
-            "title": response.css('.qX .qZ.q_::text').extract_first(),
+            "title": response.css('.rW .rY.rZ::text').extract_first(),
             "price": price_data(response),
             "promo_price": promo_price_data(response),
             'url': response.request.url,
